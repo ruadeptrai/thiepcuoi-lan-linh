54aa05490e95cd4d1e97dfa076b7668cb84026ea
LanTran_gg <60030773+ruadeptrai@users.noreply.github.com>
2026-05-23 14:17:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Dán link Web App của Google Apps Script vào giữa 2 dấu nháy dưới đây.
 // Ví dụ: window.WEDDING_RSVP_ENDPOINT = 'https://script.google.com/macros/s/AKfycb.../exec';
-window.WEDDING_RSVP_ENDPOINT = 'https://script.google.com/macros/s/AKfycbx8u02K7qCIuwAobo1UCnoBlZxFCUIcrHEwA4tF2OwOD6O61RQDNaBgzktCItUK9Ibi/exec';
+window.WEDDING_RSVP_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxx_vgnlZN65Bd-vW-3uLhqT2dxwJDCtfgdCAFXQPOdEnbLUV1aIVDmdGrPOQsOujeN/exec';
